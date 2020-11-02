93fc705e8a03f17ada634540ad83a47f3b5ac403
Max Mecklin <max@meckl.in>
2020-11-02 21:36:35 +0200

    Added more tests for add.js

--- test/add.test.js
+++ test/add.test.js
@@ -7,3 +7,11 @@ import add from '../src/add'
 test('add numbers', () => {
     expect(add(1, 2)).toBe(3)
 })
+
+test('add negative numbers', () => {
+    expect(add(-1, -1)).toBe(-2);
+})
+
+test('add infinite', () => {
+    expect(add(Infinity, 5)).toBe(Infinity);
+})
