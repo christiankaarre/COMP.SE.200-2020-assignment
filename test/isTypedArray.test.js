282a011b46e32451587f9e31d751fe4a9fa88a69
Max Mecklin <max@meckl.in>
2020-11-05 20:04:01 +0200

    Implemented tests for isTypedArray

---
+++ test/isTypedArray.test.js
@@ -0,0 +1,16 @@
+import isTypedArray from "../src/isTypedArray";
+import { test } from "@jest/globals";
+
+test('empty array is not a typed array', () => {
+   expect(isTypedArray([]))
+       .toBeDefined()
+       .toBe(false);
+});
+
+test('uint 8 array is a typed array', () => {
+    let array = new Uint8Array([1, 2, 3, 4, 5]);
+    expect(isTypedArray(array))
+        .toBeDefined()
+        .toBe(true);
+});
+
