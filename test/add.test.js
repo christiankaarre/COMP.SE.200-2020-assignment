fe26088799663dee19e59c08fd5a450d4657580a
Christian Kaarre <christian.kaarre@tuni.fi>
2020-11-14 13:26:40 +0200

    Delete unnecessary commented lines

--- test/add.test.js
+++ test/add.test.js
@@ -1,8 +1,4 @@
-// beforeEach(() =>  {
-//     jest.resetModules();
-// });
 import add from '../src/add'
-//const add = require('../src/add');
 
 test('add numbers', () => {
     expect(add(1, 2)).toBe(3)
