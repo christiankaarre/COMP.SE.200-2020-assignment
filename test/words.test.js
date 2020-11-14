f0febcf3991195ff1dc07568277330d0ad09caa9
Max Mecklin <max@meckl.in>
2020-11-14 18:55:08 +0200

    Implemented test for words

---
+++ test/words.test.js
@@ -0,0 +1,26 @@
+import words from "../src/words";
+import { test } from "@jest/globals";
+
+test('words from string', () => {
+    expect(words('fred, barney, & pebbles'))
+        .toBeDefined()
+        .toStrictEqual(['fred', 'barney', 'pebbles']);
+});
+
+test('words from string with matching', () => {
+    expect(words('fred, barney, & pebbles', /[^, ]+/g))
+        .toBeDefined()
+        .toStrictEqual(['fred', 'barney', '&', 'pebbles']);
+});
+
+test('words from empty string', () => {
+    expect(words(""))
+        .toBeDefined()
+        .toStrictEqual([]);
+});
+
+test('words from empty string with matching', () => {
+    expect(words("", /[^, ]+/g))
+        .toBeDefined()
+        .toStrictEqual([]);
+});
