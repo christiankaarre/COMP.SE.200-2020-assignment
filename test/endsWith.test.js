eb61e5f2508480e998e9bc0e31d9d843bd21f762
Christian Kaarre <christian.kaarre@tuni.fi>
2020-11-14 16:21:40 +0200

    Implement test cases for endsWith.js

---
+++ test/endsWith.test.js
@@ -0,0 +1,21 @@
+import endsWith from '../src/endsWith'
+
+test('endsWith', () => {
+    expect(endsWith('abc', 'c')).toBe(true)
+})
+
+test('endsWith', () => {
+    expect(endsWith('abc', 'b')).toBe(false)
+})
+
+test('endsWith', () => {
+    expect(endsWith('abc', 'b', 2)).toBe(true)
+})
+
+test('endsWith', () => {
+    expect(endsWith('abc', 'c', 4)).toBe(true)
+})
+
+test('endsWith', () => {
+    expect(endsWith('abc', 'a', -1)).toBe(false)
+})
