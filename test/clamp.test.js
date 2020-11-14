c646c366921f4f4d809e8e70cb2c539f0858ef74
Christian Kaarre <christian.kaarre@tuni.fi>
2020-11-14 15:55:20 +0200

    Implement test cases for clamp.js

---
+++ test/clamp.test.js
@@ -0,0 +1,25 @@
+import clamp from '../src/clamp'
+
+test('Clamp with positive and negative numbers', () => {
+    expect(clamp(10, -5, 5)).toBe(5)
+})
+
+test('Clamp with mostly negative numbers', () => {
+    expect(clamp(-10, -5, 5)).toBe(-5)
+})
+
+test('Clamp with zeros', () => {
+    expect(clamp(5, 0, 0)).toBe(0)
+})
+
+test('Clamp with reversed boundaries', () => {
+    expect(clamp(5, 5, -5)).toBe(5)
+})
+
+test('Clamp with incorrect types', () => {
+    expect(clamp(5, 'a', 'b')).toBe(0)
+})
+
+test('Clamp with incorrect types', () => {
+    expect(clamp('c', 'a', 'b')).toBe(0)
+})
