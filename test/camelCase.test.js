c294e62c1e61e0ddec45898dcfe822d2af11d80a
Max Mecklin <max@meckl.in>
2020-11-14 17:41:48 +0200

    Implemented tests for camelCase

---
+++ test/camelCase.test.js
@@ -0,0 +1,20 @@
+import camelCase from "../src/camelCase";
+import { test } from "@jest/globals";
+
+test('Foo Bar to camel case', () => {
+    expect(camelCase('Foo Bar'))
+        .toBeDefined()
+        .toBe('fooBar');
+});
+
+test('--foo-bar-- to camel case', () => {
+    expect(camelCase('--foo-bar--'))
+        .toBeDefined()
+        .toBe('fooBar');
+});
+
+test('__FOO_BAR__ to camel case', () => {
+    expect(camelCase('__FOO_BAR__'))
+        .toBeDefined()
+        .toBe('fooBar');
+});
