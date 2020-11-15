c41fb0838c829f01cece53ba2a34c3601af3349e
Christian Kaarre <christian.kaarre@tuni.fi>
2020-11-15 12:37:14 +0200

    Remove unnecessary lines from isBuffer.js

--- test/isBuffer.test.js
+++ test/isBuffer.test.js
@@ -8,11 +8,34 @@ test('Allocated Buffer', () => {
         .toBe(true);
 });
 
+test('Allocated Uint8Array Buffer', () => {
+    let x = new Buffer(new Uint8Array(2))
+    expect(isBuffer(x))
+        .toBeDefined()
+        .toBe(true);
+});
+
+test('Allocated allocUnsafe', () => {
+    let x = Buffer.allocUnsafe(10)
+    expect(isBuffer(x))
+        .toBeDefined()
+        .toBe(true);
+});
+
 test('Allocated Uint8Array', () => {
     let x = new Uint8Array(2)
     expect(isBuffer(x))
         .toBeDefined()
         .toBe(false);
 });
+
+test('Allocated unfedined variable', () => {
+    let x = undefined
+    expect(isBuffer(x))
+        .toBeDefined()
+        .toBe(false);
+});
+
+
 
 
